56a15e5c3911f9bb4ad3d7fc2bf0430b301cb023
Justin OConnell <itsjustinoconnell@gmail.com>
2024-08-08 13:34:01 +0100

    fixed alias path issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1 className="text-3xl underline font-bold text-red-500">Hello World</h1>
+    <>
+      <h1 className="text-3xl underline font-bold text-red-500">Hello World</h1>
+    </>
   );
 }
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dir, "./src"),
-      "@server": path.resolve(import.meta.dir, "../server"),
+      "@": path.resolve(__dirname, "./src"),
+      "@server": path.resolve(__dirname, "../../server"),
     },
   },
 });
