f22b5891cc80577b68d3801c556de12f0c73e06b
Justin OConnell <itsjustinoconnell@gmail.com>
2024-08-08 11:23:00 +0100

    expense post route - data validated, type created from schema

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 - [x] Server file, hono instance and create server
 - [x] - base api path
 - [x] expenses route - get expenses, create expense, update expense, delete expense
-- [] TODO: - zod validator for post
-- [] TODO: create zod schema, create type from schema with infer
+- [x] zod validator for post
+- [x] create zod schema, create type from schema with infer
 - [] TODO: Hono RPC for typesafety
 - [] TODO: Static serve frontend build files - root and fallback
 - [] TODO: Create middleware to set a user var
--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,8 +1,25 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string(),
+  amount: z.number(),
+});
+
+type ExpenseSchema = z.infer<typeof schema>;
 // Create base path for expenses route
 export const expensesRoutes = new Hono()
   .get("/", (c) => {
     return c.json({ message: "expense" });
   })
-  .post("/", (c) => c.json({ message: "new expense" }))
+  .post(
+    "/",
+    zValidator("json", schema, (result, c) => {
+      if (!result.success) {
+        return c.json(result.error.flatten());
+      }
+      return c.json({ result });
+    })
+  )
   .delete("/", (c) => c.json({ message: "expense deleted" }));
